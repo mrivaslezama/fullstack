2a82c38a6286de9aec55fcee783ae4de3719f9f6
Brayan Diaz C <brayandiazc@gmail.com>
2022-11-11 15:38:17 -0500

    doc: Fake data to tweets

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,17 @@ puts 'Creating 200 fake articles...'
     published: Faker::Boolean.boolean
   )
 end
+
+require 'faker'
+
+Tweet.destroy_all
+
+puts 'Creating 200 fake tweets...'
+
+200.times do
+  Tweet.create(
+    title: Faker::Book.title,
+    content: Faker::Lorem.paragraph,
+    published: Faker::Boolean.boolean
+  )
+end
